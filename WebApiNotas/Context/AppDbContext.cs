f662d38ebf97662807755a9a55bc003bd0d63324
JesusDHernandezR <jesusdavidhernandez@unicesar.edu.co>
2024-04-12 02:17:02 -0500

    Agregar archivos de proyecto.

---
+++ WebApiNotas/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiNotas.Models;
+
+namespace WebApiNotas.Context
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+            
+        }
+
+        public DbSet<Note> Notes { get; set; }
+    }
+}
---
+++ WebApiNotas/Models/Note.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApiNotas.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public DateTime Fecha { get; set; }
+        public required string Titulo { get; set; }
+        public required string Categoria { get; set; }    
+        public required string Descripcion { get; set;}
+    }
+}
